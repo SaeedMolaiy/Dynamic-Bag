29fb33bd67cbbd8f3e63c8e2f982ca861199356b
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-01-11 15:07:53 +0330

    Dynamic Bag

---
+++ DynamicBag/Model/DynamicBag.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Dynamic;
+
+namespace DynamicBag.Model
+{
+    public class DynamicBag : DynamicObject
+    {
+        private readonly Dictionary<string, dynamic> _members;
+
+        public DynamicBag()
+        {
+            _members = new Dictionary<string, dynamic>(StringComparer.InvariantCultureIgnoreCase);
+        }
+
+        // If you try to get a value of a member
+        // not defined in the class, this method is called.
+        public override bool TryGetMember(GetMemberBinder binder, out dynamic member)
+        {
+            // Converting the member name to lowercase
+            // so that member names become case-insensitive.
+            var binderName = binder.Name.ToLower();
+
+            // If the member name is found in a dictionary,
+            // set the out member parameter to the member value and return true.
+            // Otherwise, return false.
+            member = _members[binderName];
+
+            return member != null;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, dynamic value)
+        {
+            // Converting the member name to lowercase
+            // so that member names become case-insensitive.
+            var binderName = binder.Name.ToLower();
+
+            if (value == null)
+            {
+                if (_members.ContainsKey(binderName))
+                {
+                    _members.Remove(binderName);
+                }
+            }
+            else
+            {
+                _members[binderName] = value;
+            }
+            // You can always add a value to a dictionary,
+            // so this method always returns true.
+            return true;
+        }
+
+    }
+}
---
+++ DynamicBag/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DynamicBag
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            dynamic bag = new Model.DynamicBag();
+
+            bag.FirstName = "Saeed";
+            bag.LastName = "Molaiy";
+
+            Console.WriteLine($"Author Full Name is : {bag.FirstName} {bag.LastName}");
+
+            Console.ReadKey();
+        }
+    }
+}
